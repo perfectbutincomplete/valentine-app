383887b163cc0f200031c3ce24de9b31df447c08
goodbutbad <slimecraft2229@gmail.com>
2026-02-13 18:30:53 +0600

    Updates

--- app/immutable/chunks/ChqkMMij.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_1sb61bw?.base??"/valentine-app",a=globalThis.__sveltekit_1sb61bw?.assets??s??"";export{a,s as b};
---
+++ app/immutable/chunks/CpSE531D.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_1b7ou9r?.base??"",a=globalThis.__sveltekit_1b7ou9r?.assets??s??"";export{a,s as b};
